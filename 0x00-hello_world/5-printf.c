5b58218b3383e923e1bd040f4c0bf9ae4d96ea02
Marisstella <jenniephar@gmail.com>
2023-03-16 19:35:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#nclude < stdio.h >
+/**
+ * main - A program that print a line using the printf function
+ * Return 0 (Success)
+ */
+
+init main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,n\")
+	return (0);
+}
+
+
