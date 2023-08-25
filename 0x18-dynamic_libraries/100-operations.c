72cc0d81d1d213b6c5b5cf8a420108c32a67114a
Marisstella <jenniephar@gmail.com>
2023-08-25 00:12:55 -0700

    task 2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -13,7 +13,19 @@ float divide(float a, float b) {
 	if (b != 0) { 
 		return a / b;
 	} else { 
-		printf("Error: Division by zero\n");
+		fprintf(stderr, "Division by zero!\n");
 		return 0;
 	} 
+
+int mod(int a, int b)
+{
+		if (b != 0)
+		{
+		return a % b;
+		}
+	else
+		{
+			fprintf(stderr, "Modulo by zero!\n");
+			return 0;  // Return some default value in case of modulo by zero
+		}
 }
