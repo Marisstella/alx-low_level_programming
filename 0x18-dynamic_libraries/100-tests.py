9cbec4b0a12a4a97f8a592d544eb83324463d700
Marisstella <jenniephar@gmail.com>
2023-08-24 20:27:38 -0700

    task 2

---
+++ 0x18-dynamic_libraries/100-tests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import random
+import ctypes
+
+# Load the dynamic library
+cops = ctypes.CDLL('./100-operations.so')
+
+# Define function argument and return types
+cops.add.argtypes = [ctypes.c_int, ctypes.c_int]
+cops.add.restype = ctypes.c_int
+cops.sub.argtypes = [ctypes.c_int, ctypes.c_int]
+cops.sub.restype = ctypes.c_int
+cops.mul.argtypes = [ctypes.c_int, ctypes.c_int]
+cops.mul.restype = ctypes.c_int
+cops.div.argtypes = [ctypes.c_int, ctypes.c_int]
+cops.div.restype = ctypes.c_int
+cops.mod.argtypes = [ctypes.c_int, ctypes.c_int]
+cops.mod.restype = ctypes.c_int
+
+# Generate random numbers
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+
+# Call C functions and print results
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
