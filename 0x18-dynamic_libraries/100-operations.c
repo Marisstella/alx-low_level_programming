0773a7b2acd60ab2c1845e444f2d8dcb1447b9dc
Marisstella <jenniephar@gmail.com>
2023-08-25 00:20:34 -0700

    task 2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
-int add(int a, int b) { 
+int add(int a, int b) 
+{
 	return a + b;
 }
-int subtract(int a, int b) {
+
+int sub(int a, int b) 
+{
 	return a - b;
 }
-float multiply(float a, float b) {
+
+int mul(int a, int b) 
+{
 	return a * b;
 }
-float divide(float a, float b) {
-	if (b != 0) { 
+
+int div(int a, int b) 
+{
+	if (b != 0)
+	{
 		return a / b;
-	} else { 
+	}
+	else
+	{
 		fprintf(stderr, "Division by zero!\n");
-		return 0;
-	} 
+		return 0;  // Return some default value in case of division by zero
+	}
+}
 
 int mod(int a, int b)
 {
